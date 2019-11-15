8442d73d4342ed9c5ae2d315fbf3533b415c4c8d
Eugene Havrylov <eugeneh@dev6.com>
2019-11-14 19:47:42 -0500

    deployment set up

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,5 +59,11 @@ module.exports = {
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
   ],
 }
